d20177877a457abdffbd9c934ef61ccd9b3e00f9
Allen Jang <e1668058@cmaisonneuve.qc.ca>
2020-04-14 15:34:02 -0400

    First

---
+++ precache-manifest.2e6e96fee91288170b68aa5a49e5f3dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4566dcc2ce1f8ea9fede6f0ad7337b04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "399e4b1dd02b472b60d0",
+    "url": "/static/css/main.feb11f45.chunk.css"
+  },
+  {
+    "revision": "92134cfb05ac1ce905d2",
+    "url": "/static/js/2.1c2698b3.chunk.js"
+  },
+  {
+    "revision": "973b0157c75ad00a2bf6d9ba8bf3685b",
+    "url": "/static/js/2.1c2698b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "399e4b1dd02b472b60d0",
+    "url": "/static/js/main.2ce37d87.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
